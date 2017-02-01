6a0931bc51ad16e36a2824f5dc39efb53825b06a
Ivan <you@example.com>
2017-02-01 02:36:52 +0200

    hometask 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,91 @@
-angular.module('testApp', [])
-    .controller('TextBlockController', function() {
-        this.visible = true;
+(function() {
+    angular
+        .module('testApp', [])
+        .controller('MainController', ['$scope', function($scope) {
 
-        this.hideShow = function(){
-            this.visible = !this.visible;
-        }
-    });
+            $scope.countPerPage = 5;
+            $scope.offset = 0;
+            $scope.pageIndex = 0;
+            $scope.checkAll = 0;
+
+            // angular.fromJson(window.localStorage.getItem('taskList')) || []
+            $scope.newTaskName = '';
+            $scope.taskList = angular.fromJson(window.localStorage.getItem('taskList')) || [];
+
+            paginateDebug($scope);
+
+            $scope.pageCount = getPageCount($scope);
+            //$scope.currentPage = $scope.countPerPage * $scope.offset;
+
+            $scope.addTask = function() {
+                if ($scope.newTaskName) {
+                    $scope.taskList.push({
+                        id: $scope.taskList.length,
+                        name: $scope.newTaskName,
+                        done: false
+                    });
+                    $scope.newTaskName = '';
+                }
+            };
+
+            $scope.removeTask = function(taskId) {
+                for (var i = 0; i < $scope.taskList.length; i++) {
+                    if ($scope.taskList[i].id === taskId) {
+                        if (confirm('Are you sure you want to delete item `' + $scope.taskList[i].name + '`')) {
+                            $scope.taskList.splice(i, 1);
+                        }
+                        break;
+                    }
+                }
+            };
+
+            $scope.removeAllPageTasks = function() {
+                $scope.taskList.splice($scope.pageIndex * $scope.countPerPage, $scope.countPerPage);
+            };
+
+            $scope.checkAllPageTasks = function(checked) {
+                for (var i = $scope.pageIndex * $scope.countPerPage; i < $scope.countPerPage * ($scope.pageIndex + 1); i++) {
+                    $scope.taskList[i].done = checked;
+                }
+            };
+
+            $scope.range = function(n) {
+                return new Array(n);
+            };
+
+            $scope.setPage = function(pageIndex) {
+                $scope.pageIndex = pageIndex;
+                $scope.currentPage = $scope.countPerPage * pageIndex;
+            };
+
+            $scope.$watch('taskList', function(newVal, oldVal) {
+                if (newVal !== oldVal) {
+                    window.localStorage.setItem('taskList', angular.toJson($scope.taskList));
+                }
+
+                $scope.pageCount = getPageCount($scope);
+            }, true);
+
+
+            /**
+             *
+             * @param $scope
+             */
+            function paginateDebug($scope){
+                console.log('length =', $scope.taskList.length);
+                console.log('perPage =',  $scope.countPerPage);
+                console.log('offset =',  $scope.offset);
+            }
+
+            /**
+             *
+             * @param $scope
+             * @returns {number}
+             */
+            function getPageCount($scope){
+                return Math.ceil($scope.taskList.length / $scope.countPerPage);
+            }
+
+        }]);
+
+})();
